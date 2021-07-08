323327ea8b82835a106dafc0c25e9074e5f8fb97
Riteshjain1 <>
2021-07-08 14:13:25 +0530

    Changes made to config file and ports

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
+        const con = await mongoose.connect("mongodb+srv://admin:admin123@cluster0.a8qqs.mongodb.net/listDB", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
